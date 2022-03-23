8cc5252209a21d684258a727a2b5aa4e70a12499
marcosRoos <marcos.roos.1958@gmail.com>
2022-03-23 01:26:44 -0300

    implemented client on main

--- APP/App.java
+++ APP/App.java
@@ -1,17 +1,10 @@
-import com.roos.oop.lesson1.Account;
-import com.roos.oop.lesson1.Lamp;
+import com.roos.oop.lesson2.Client;
 
 class App {
     
     public static void main(String[] args) {
-        boolean originalState = false;
 
-        Lamp lamp = new Lamp(originalState);
-        System.out.println("A new lamp was created, and it is " + (originalState ? "on" : "off") + ".");
-        lamp.test();
-
-        Account account = new Account( "0123 4567 8909 0123" , 2000);
-        account.test();
+        Client client = new Client();
 
     }
 
