155470ac988d0fb002e43709091e584372113d43
marcosRoos <marcos.roos.1958@gmail.com>
2022-03-23 01:23:20 -0300

    added contact variants: phone and email

---
+++ APP/com/roos/oop/lesson2/Email.java
@@ -0,0 +1,8 @@
+package com.roos.oop.lesson2;
+
+public class Email extends Contact {
+
+    public Email(String value) {
+        super(value, ContactTypes.email);
+    }
+}
---
+++ APP/com/roos/oop/lesson2/Phone.java
@@ -0,0 +1,8 @@
+package com.roos.oop.lesson2;
+
+public class Phone extends Contact {
+
+    public Phone(String value, ContactTypes type) {
+        super(value, type);
+    }
+}
